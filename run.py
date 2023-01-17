9756999d16eaae9b95dc3b27a140b438ee173d73
Vcore52s <86291879+Vcore52s@users.noreply.github.com>
2023-01-17 18:49:57 +0900

    Add files via upload

---
+++ run.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+from tortoise import Tortoise
+import os
+
+bot = commands.Bot(command_prefix="!")
+bot.remove_command("help")
+bot.load_extension("poll")
+
+
+@bot.event
+async def on_ready():
+    await Tortoise.init(db_url="sqlite://db.sqlite3", modules={"models": ["model"]})
+    await Tortoise.generate_schemas()
+    print("Ready")
+
+
+TOKEN = os.environ['TOKEN']
